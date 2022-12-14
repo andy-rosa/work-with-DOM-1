f2412720aeff4b63c41a6b3f72c8187cc7d295a9
Andrey Shi <andy.roshitu@gmail.com>
2022-12-14 16:49:59 +0300

    Добавляет переключение темы

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-/* JavaScript код */
+'use strict';
+
+const buttonThemeLight = document.querySelector('.theme-button-light');
+const buttonThemeDark = document.querySelector('.theme-button-dark');
+const buttonThemeTexture = document.querySelector('.theme-button-texture');
+
+
+const buttonThemeLightHandler = () => {
+  document.documentElement.dataset['themeName'] = 'light';
+};
+
+const buttonThemeDarkHandler = () => {
+  document.documentElement.dataset['themeName'] = 'dark';
+};
+
+const buttonThemeTextureHandler = () => {
+  document.documentElement.dataset['themeName'] = 'texture';
+};
+
+const init = () => {
+  buttonThemeLight.addEventListener('click', buttonThemeLightHandler);
+  buttonThemeDark.addEventListener('click', buttonThemeDarkHandler);
+  buttonThemeTexture.addEventListener('click', buttonThemeTextureHandler);
+};
+
+init();
